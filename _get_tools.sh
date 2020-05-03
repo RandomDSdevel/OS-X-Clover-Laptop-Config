f069ae589282ab8f2da63faeb339172b7e05920f
Bryce Glover <RandomDSdevel@gmail.com>
2020-05-03 15:44:20 -0400

    _get_tools.sh:  Redirect first submodule check error output to standard error.
         Otherwise, that check would output:
    
    ```
    fatal: cannot change to './_tools/': No such file or directory
    ```
    
    on first use (when said folder doesn't already exist.)

--- _get_tools.sh
+++ _get_tools.sh
@@ -2,7 +2,7 @@
 #set -x
 
 # get copy of tools if not present
-if [[ ! -d ./_tools && -z "$(git -C ./_tools/ rev-parse --show-superproject-working-tree)" ]]; then
+if [[ ! -d ./_tools && -z "$(git -C ./_tools/ rev-parse --show-superproject-working-tree 2> /dev/null)" ]]; then
     git submodule add -f --name hack-tools https://github.com/RehabMan/hack-tools.git ./_tools
     git submodule update --init ./_tools
 fi
